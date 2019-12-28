92b175f23f92a4b3279c1f6b775639bdbaf19d9a
Federico Carrillo Chaves <fedemeister@outlook.es>
2019-12-28 16:58:21 +0100

    all data fixed for t0-t1

---
+++ T0-T1/mobile_plans.sql
@@ -0,0 +1,22 @@
+CREATE TABLE mobile_plans(
+   mb_id              INTEGER  NOT NULL PRIMARY KEY 
+  ,mb_name            VARCHAR(12) NOT NULL
+  ,mb_expiration_date INTEGER  NOT NULL
+  ,mb_launch_date     INTEGER  NOT NULL
+);
+
+INSERT INTO mobile_plans(mb_id,mb_name,mb_expiration_date,mb_launch_date) VALUES (0,'Magneta Plus',719,4);
+INSERT INTO mobile_plans(mb_id,mb_name,mb_expiration_date,mb_launch_date) VALUES (1,'Magneta',409,363);
+INSERT INTO mobile_plans(mb_id,mb_name,mb_expiration_date,mb_launch_date) VALUES (2,'Magneta Plus',563,304);
+INSERT INTO mobile_plans(mb_id,mb_name,mb_expiration_date,mb_launch_date) VALUES (3,'Magneta Plus',329,174);
+INSERT INTO mobile_plans(mb_id,mb_name,mb_expiration_date,mb_launch_date) VALUES (4,'Essential',422,218);
+INSERT INTO mobile_plans(mb_id,mb_name,mb_expiration_date,mb_launch_date) VALUES (5,'Magneta Plus',413,230);
+INSERT INTO mobile_plans(mb_id,mb_name,mb_expiration_date,mb_launch_date) VALUES (6,'Essential',474,4);
+INSERT INTO mobile_plans(mb_id,mb_name,mb_expiration_date,mb_launch_date) VALUES (7,'Essential',719,322);
+INSERT INTO mobile_plans(mb_id,mb_name,mb_expiration_date,mb_launch_date) VALUES (8,'Magneta Plus',713,199);
+INSERT INTO mobile_plans(mb_id,mb_name,mb_expiration_date,mb_launch_date) VALUES (9,'Essential',416,119);
+INSERT INTO mobile_plans(mb_id,mb_name,mb_expiration_date,mb_launch_date) VALUES (10,'Magneta',433,246);
+INSERT INTO mobile_plans(mb_id,mb_name,mb_expiration_date,mb_launch_date) VALUES (11,'Magneta Plus',418,283);
+INSERT INTO mobile_plans(mb_id,mb_name,mb_expiration_date,mb_launch_date) VALUES (12,'Essential',662,86);
+INSERT INTO mobile_plans(mb_id,mb_name,mb_expiration_date,mb_launch_date) VALUES (13,'Essential',626,132);
+INSERT INTO mobile_plans(mb_id,mb_name,mb_expiration_date,mb_launch_date) VALUES (14,'Essential',262,204);
